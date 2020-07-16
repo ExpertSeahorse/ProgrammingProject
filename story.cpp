9d9ea6b9c22d0c500fd081529604af086a781228
ExpertSeahorse <dtfeldman@verizon.net>
2020-07-16 13:16:17 -0400

    Various edits

--- story.cpp
+++ story.cpp
@@ -1,5 +1,7 @@
 #include "story.h"
 
+#include <sstream>
+
 // if no input file chosen
 Story::Story(){
     cout << "You must pass a filename!" << endl;
@@ -34,9 +36,10 @@ Story::Story(string filename){
 }
 
 // Prints out all passages in order of appearence
-void Story::print(){
+string Story::print(){
+    ostringstream ooutstring;
     for (auto i : passages_vec){
-        cout << "Passage " << i.getName() << ":" << endl;
+        ooutstring << "Passage " << i.getName() << ":" << endl;
 
         PassageTokenizer pt(i.getText());
         while (pt.hasNextPart()){
@@ -44,11 +47,11 @@ void Story::print(){
             
             switch (stok.getType()){
             case TEXT:
-                cout << "  Text:  \"" << stok.getText() << "\"" << endl;
+                ooutstring << "Text:  \"" << stok.getText() << "\"" << endl;
                 break;
 
             case LINK:{
-                cout << "  Link:  display=";
+                ooutstring << "Link:  display=";
                 string text = stok.getText();
 
                 // Test if display != target
@@ -56,28 +59,28 @@ void Story::print(){
                 if (found != string::npos){
 
                     // If test != target: 
-                    cout << text.substr(2, found-2) << ", target=";
-                    cout << text.substr(found+5, text.size()-(found+5+2)) << endl;
+                    ooutstring << text.substr(2, found-2) << ", target=";
+                    ooutstring << text.substr(found+5, text.size()-(found+5+2)) << endl;
                 }
                 else{
                     // If test == target:
                     string s = text.substr(2, text.size()-4);
-                    cout << s << ", target=" << s << endl;
+                    ooutstring << s << ", target=" << s << endl;
                 }
                 break;
             }
 
             case GOTO:{
-                cout << "  Go-to:  target=";
+                ooutstring << "Goto:  target=";
                 string text = stok.getText();
                 
                 // Print all chars after "(go-to: &quot;" until the next &
-                cout << text.substr(14, text.find('&')) << endl;
+                ooutstring << text.substr(14, text.find('&')) << endl;
                 break;
             }
 
             case SET:{
-                cout << "  Set: var=";
+                ooutstring << "Set:  var=";
                 string text = stok.getText();
                 
                 // Will cut out everything before '$'
@@ -91,20 +94,21 @@ void Story::print(){
                 size_t found = text.find("true");
                     
                 // Will output variable and value and assign bool 
-                cout << var << ", ";
+                ooutstring << var << ", ";
                     
                 if(found != string::npos) {
                     val = true;
-                    cout << "value=[" << val << "]" << endl;
+                    ooutstring << "value=true" << endl;
+                    //ooutstring << "value=[" << val << "]" << endl;
                 }
                 else {
                     val = false;
-                    cout << "value=[" << val << "]" << endl;
+                    ooutstring << "value=false" << endl;
                 }
                 break;
             }
             case IF: {
-                cout << "  If: var=";
+                ooutstring << "If:  var=";
                 string text = stok.getText();
                 
                 //Will cut out all characters before '$'
@@ -119,20 +123,20 @@ void Story::print(){
                 size_t found = text.find("true");
                     
                 // Will output variable and value and assign bool 
-                cout << var << ", ";
+                ooutstring << var << ", ";
                     
                 if(found != string::npos) {
                     val = true;
-                    cout << "value=true" << endl;
+                    ooutstring << "value=true" << endl;
                 }
                 else {
                     val = false;
-                    cout << "value=false" << endl;
+                    ooutstring << "value=false" << endl;
                 }
                 break;
             }
             case ELSEIF: {
-                cout << "  Else-if:  var=";
+                ooutstring << "Else-if:  var=";
                 string text = stok.getText();
             
                 //Will cut out all characters before '$'
@@ -147,22 +151,22 @@ void Story::print(){
                 size_t found = text.find("true");
                     
                 // Will output variable and value and assign bool
-                cout << var << ", ";
+                ooutstring << var << ", ";
                     
                 if(found != string::npos) {
                     val = true;
-                    cout << "value=true" << endl;
+                    ooutstring << "value=true" << endl;
                 }
                 else {
                     val = false;
-                    cout << "value=false" << endl;
+                    ooutstring << "value=false" << endl;
                 }
                 break;
             }
 
             case ELSE: {
                 // Will output else
-                cout << "  Else  "<< endl;
+                ooutstring << "Else"<< endl;
                 break;
             }
             case BLOCK:{
@@ -182,98 +186,100 @@ void Story::print(){
                 // it will break out completely when program has iterated through entire text
       
                 while(i < text.size()) {
-                // for loop is used to iterate through the text
+                    // for loop is used to iterate through the text
                 // 4 if statements in the for loop will output the start of a block if
                 // '[' is found and will out put the end of the block if ']' is found
                 // loop will break if the next character is an alphabetical letter or
                 // a '(' and assign the bool for words indicating text or para for parantheses
                     
-                for (; i < text.size(); i++) {
-                    if(text[i] == '['){
-                        cout << "START BLOCK" << endl;
-                    }
-                    if(text[i] == ']'){
-                        cout << "END BLOCK" << endl;
-                    }
-                    if(isalpha(text[i])) {
-                        words = true;
-                        break;
-                    }
-                    if(text[i] == '(') {
-                        para = true;
-                        break;
-                    }
-                }
-                        
-                // if statements are used with the bool values as only one bool value will be on at any
-                // given time.
-                    
-                if(words == true) {
-                    //j will be set equal to i as that is the beginning of the text in the previous for loop
-                    //this for loop will look for the end of text by finding the corresponding symbols for
-                    //brackets or parantheses and i and j will be used to cut the original text and output the text
-         
-                    for (j = i; j < text.size(); j++) {
-                        if(text[j] == '(' || text[j] == '[' || text[j] == ']') {
+                    for (; i < text.size(); i++) {
+                        if(text[i] == '['){
+                            ooutstring << "START BLOCK" << endl;
+                        }
+                        if(text[i] == ']'){
+                            ooutstring << "END BLOCK" << endl;
+                        }
+                        if(isalpha(text[i])) {
+                            words = true;
+                            break;
+                        }
+                        if(text[i] == '(') {
+                            para = true;
                             break;
                         }
                     }
-                    string newtext = text.substr(i, j - i);
-                    cout << "Text: \"" << newtext << '\"' << endl;
-                    
-                    // words is set to false so that the original for loop can activate it again if there are more than one
-                    // text passage
-                    // i is also set to j because that would be our new starting point in the original text due to the fact
-                    // that we output all the text until j
-                    words = false;
-                    i = j;
-                }
-
-                if(para == true) {
-                    // for loop will set j to i as that is the beginning of the parantheses text
-                    // the for loop will iterate until it finds a closing ')' and break
-                    // the original text will be cut using i and j as delimeters for newtext
-                    for (j = i; j < text.size(); j++) {
-                        if(text[j] == ')') {
-                        break;
+                        
+                    // if statements are used with the bool values as only one bool value will be on at any
+                    // given time.
+                        
+                    if(words == true) {
+                        //j will be set equal to i as that is the beginning of the text in the previous for loop
+                        //this for loop will look for the end of text by finding the corresponding symbols for
+                        //brackets or parantheses and i and j will be used to cut the original text and output the text
+            
+                        for (j = i; j < text.size(); j++) {
+                            if(text[j] == '(' || text[j] == '[' || text[j] == ']') {
+                                break;
+                            }
                         }
+                        string newtext = text.substr(i, j - i);
+                        ooutstring << "Text:  \"" << newtext << '\"' << endl;
+                        
+                        // words is set to false so that the original for loop can activate it again if there are more than one
+                        // text passage
+                        // i is also set to j because that would be our new starting point in the original text due to the fact
+                        // that we output all the text until j
+                        words = false;
+                        i = j;
                     }
-                    
-                    string newtext = text.substr(i, j - (i - 1));
-                    string var = newtext.substr(newtext.find("$"));
-                    bool val;    
-                    
-                    //Will cut the newtext for var starting with '$" and ending with
-                    //the next whitespace character
-                    var = var.substr(var.find("$"), var.find(" "));
-                
-                    //test to search is "true" is in the parantheses or not
-                    size_t found = text.find("true");
-                    
-                    // will output var value if true or false
-                    cout << "If: var=" << var << ", ";
 
-                    if(found != string::npos) {
-                        val = true;
-                        cout << "value=true" << endl;
+                    if(para == true) {
+                        // for loop will set j to i as that is the beginning of the parantheses text
+                        // the for loop will iterate until it finds a closing ')' and break
+                        // the original text will be cut using i and j as delimeters for newtext
+                        for (j = i; j < text.size(); j++) {
+                            if(text[j] == ')') {
+                            break;
+                            }
+                        }
+                        
+                        string newtext = text.substr(i, j - (i - 1));
+                        string var = newtext.substr(newtext.find("$"));
+                        bool val;    
+                        
+                        //Will cut the newtext for var starting with '$" and ending with
+                        //the next whitespace character
+                        var = var.substr(var.find("$"), var.find(" "));
+                    
+                        //test to search is "true" is in the parantheses or not
+                        size_t found = text.find("true");
+                        
+                        // will output var value if true or false
+                        ooutstring << "If:  var=" << var << ", ";
+
+                        if(found != string::npos) {
+                            val = true;
+                            ooutstring << "value=true" << endl;
+                        }
+                        else {
+                            val = false;
+                            ooutstring << "value=false" << endl;
+                        }
+                        // sets para to false for the original for loop to be able to flip it to true if another '('
+                        // is found and i is updated with j because it will be our new starting position in the string
+                        para = false;
+                        i = j;
                     }
-                    else {
-                        val = false;
-                        cout << "value=false" << endl;
-                    }
-                    // sets para to false for the original for loop to be able to flip it to true if another '('
-                    // is found and i is updated with j because it will be our new starting position in the string
-                    para = false;
-                    i = j;
                 }
-            break;
+                break;
             }
 
             default:
-                cout << "  Unknown token:  ";
+                ooutstring << "  Unknown token:  ";
             }
         }
     }
+    return ooutstring.str();
 }
 
 
--- story.h
+++ story.h
@@ -21,7 +21,7 @@ private:
 public:
     Story();
     Story(string);
-    void print();
+    string print();
     void play();
 };
 
